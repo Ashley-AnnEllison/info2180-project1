30e2e15e6dfab03347f8f87d7d9be80709e14da6
Ashley-Ann Ellison <aashleyellison51@gmail.com>
2020-11-01 18:26:54 -0500

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", () =>{
+    //const form = document.forms[0];
+    let email = document.getElementById("email").value;
+    let successMessage = "Thank you! Your email address " + email + " has been submitted to our mailing list!";
+    let errorMessage = "Please enter a valid email address";
+
+    let message = document.getElementsByClassName("message");
+
+    document.getElementsByClassName("btn").addEventListener("click", showMessage);
+
+    function showMessage() {
+        if(email == "") {
+            message.innerHTML = errorMessage;
+        } else {
+            message.innerHTML = successMessage;
+        }
+    }
+});
